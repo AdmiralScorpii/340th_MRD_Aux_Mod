296601441398d53306d43bde44c8eb533a4c8ab9
Admiral_Scorpii <moreira.christian@yahoo.com>
2024-07-02 16:27:27 -0500

    FGM-148 Damage Tweaks
    Adjusted RHSUSAF Javelin "Hit" values

---
+++ us_ammo_tweaks/config.cpp
@@ -0,0 +1,135 @@
+class CfgPatches
+{
+	class 340th_USAF_Ammunition_Modifications
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=1.78;
+		requiredAddons[]=
+		{
+			"rhsusf_c_weapons"
+		};
+		name="340th MRD RHSUSAF Ammo Modifications";
+		author="Scorpii";
+	};
+};
+
+class CfgAmmo
+{
+	
+	class BulletBase;
+	class RocketBase;
+	class GrenadeHand;
+	class GrenadeHand_stone;
+	class F_40mm_White;
+	class F_40mm_Green;
+	class F_40mm_Red;
+	class F_40mm_Cir;
+	class G_40mm_HE;
+	class MissileCore;
+	class MissileBase: MissileCore	
+	{
+		class Components;
+	};
+	class rhsusf_ammo_basic_penetrator;
+	class rhs_ammo_M_fgm148_AT: MissileBase	
+	{
+		hit=250;
+		class Direct
+		{
+		};
+		class TopDown
+		{
+		};	
+		class Cruise
+		{
+		};	
+		class Components
+		{
+			class SensorsManagerComponent
+			{
+				class Components
+				{
+					class IRSensorComponent
+					{
+						class AirTarget
+						{
+						};
+						class GroundTarget
+						{
+						};
+					};
+				};
+			};
+		};		
+	};
+	class rhs_ammo_M_fgm148_AT_penetrator: rhsusf_ammo_basic_penetrator
+	{
+		warheadName="TandemHEAT";
+		hit=270;
+		caliber=50.666698;
+	};
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
